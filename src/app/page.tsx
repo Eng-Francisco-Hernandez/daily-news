b71dacb87a7adfccff6cdbbeafb1b67ef1a866b5
Eng-Francisco-Hernandez <fjch87@gmail.com>
2023-05-18 00:55:22 -0600

    feat(ui-and-data-retrieval): worked on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,46 @@
 
 import * as React from "react";
 import DrawerLayout from "@/components/layout/DrawerLayout";
-import { Typography } from "@mui/material";
+import { Grid, Link, Typography } from "@mui/material";
+import { newsCategories } from "@/data/constants";
 
 export default function Home() {
   return (
     <DrawerLayout>
-      <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
-        non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-        tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non
-        tellus. Convallis convallis tellus id interdum velit laoreet id donec
-        ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl
-        suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod
-        quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet
-        proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras
-        tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
-        varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
-        Lorem donec massa sapien faucibus et molestie ac.
-      </Typography>
-      <Typography paragraph>
-        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-        ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar elementum
-        integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi
-        lacus sed viverra tellus. Purus sit amet volutpat consequat mauris.
-        Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-        vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra
-        accumsan in. In hendrerit gravida rutrum quisque non tellus orci ac.
-        Pellentesque nec nam aliquam sem et tortor. Habitant morbi tristique
-        senectus et. Adipiscing elit duis tristique sollicitudin nibh sit.
-        Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
-        maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
-        aliquam ultrices sagittis orci a.
-      </Typography>
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h2" component="h2" color="primary">
+            Daily news
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          {newsCategories.map((category, index) => {
+            return (
+              <Link
+                href="#"
+                className="mr-10 title-case"
+                style={{
+                  textDecoration: "none",
+                }}
+              >
+                {category}
+              </Link>
+            );
+          })}
+        </Grid>
+      </Grid>
     </DrawerLayout>
   );
 }
--- src/data/constants.tsx
+++ src/data/constants.tsx
@@ -1,12 +1,22 @@
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 export const drawerItems = [
-    {
-        title: 'Inbox',
-        icon: <InboxIcon/>
-    },
-    {
-        title: 'Mail',
-        icon: <MailIcon/>
-    }
-]
+  {
+    title: "Inbox",
+    icon: <InboxIcon />,
+  },
+  {
+    title: "Mail",
+    icon: <MailIcon />,
+  },
+];
+
+export const newsCategories = [
+  "business",
+  "entertainment",
+  "general",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
